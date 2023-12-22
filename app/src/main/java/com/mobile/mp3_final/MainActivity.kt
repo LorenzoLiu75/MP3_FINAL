ccb845c3c555f30f072d907adc352dda1cb07ed6
LorenzoLiu75 <lorenzoliu797@gmail.com>
2023-12-22 15:30:04 +0700

    update database, home fragment, main activity

--- app/src/main/java/com/mobile/mp3_final/MainActivity.kt
+++ app/src/main/java/com/mobile/mp3_final/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mobile.mp3_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mobile.mp3_final.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNav
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        navView.setupWithNavController(navController)
     }
 }
